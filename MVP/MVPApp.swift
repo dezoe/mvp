4d12924d896fe9d41ba163e225985b35bcdbb2e8
Magnus Johansson <magnus.johansson@sparkhouse.se>
2022-11-25 08:31:55 +0100

    Initial Commit

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Magnus Johansson on 2022-11-25.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
